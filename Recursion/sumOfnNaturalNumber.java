927a66e6436ea690109da4e1932238bab5474fc2
Adnan Khan <adnanuitit@gmail.com>
2026-08-30 09:15:49 +0530

    New Version of Sum of N natural number

--- Recursion/sumOfnNaturalNumber.java
+++ Recursion/sumOfnNaturalNumber.java
@@ -1,18 +1,27 @@
 package DSAPractice.Recursion;
 
 public class sumOfnNaturalNumber {
-    public void sum(int i, int num, int sum){
-        if (i>num){
-            System.out.println(sum);
-            return;
+//    public void sum(int i, int num, int sum){
+//        if (i>num){
+//            System.out.println(sum);
+//            return;
+//        }
+//        sum+=i;
+//        sum(i+1,num, sum);
+//    }
+
+    //Functional Recursion
+    public int functionalRecursion(int n){
+        if (n == 0){
+            return 0;
         }
-        sum+=i;
-        sum(i+1,num, sum);
+        return n + functionalRecursion(n-1);
     }
 
     public static void main(String[] args) {
         int num = 5, i =1, sum = 0;
         sumOfnNaturalNumber obj = new sumOfnNaturalNumber();
-        obj.sum(i,num, sum);
+//        obj.sum(i,num, sum);
+        System.out.println(obj.functionalRecursion(num));
     }
 }
