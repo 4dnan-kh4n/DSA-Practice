c61fd8de7c452d10717382ca09d2d738c422fac5
Adnan Khan <adnanuitit@gmail.com>
2026-09-03 12:54:18 +0530

    Check String is Palindrome using Recursion

---
+++ Recursion/palindromeString.java
@@ -0,0 +1,28 @@
+package DSAPractice.Recursion;
+
+public class palindromeString {
+    public boolean isPalindrome(String str, int start){
+        if (start>str.length()/2){
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(str.length()-start-1)){
+            return false;
+        }
+        return isPalindrome(str,start+1);
+    }
+
+
+    public static void main(String[] args) {
+        String str = "Madam";
+        str = str.toLowerCase();
+        int start = 0;
+        palindromeString obj = new palindromeString();
+
+        if(obj.isPalindrome(str,start)){
+            System.out.println("Yes this string is Palindrome");
+        }
+        else {
+            System.out.println("No this string is not Palindrome");
+        }
+    }
+}
--- Recursion/test.java
+++ Recursion/test.java
@@ -2,20 +2,9 @@ package DSAPractice.Recursion;
 
 public class test {
 
-//    3,2,1
-    public void test(int i, int n){
-        if (i>n){
-            return;
-        }
-        test(i+1,n);
-        System.out.println(i);
-    }
-
-
     public static void main(String[] args) {
-        test obj = new test();
-        int n = 3;
-        int i = 1;
-        obj.test(i,n);
+        String str = "Adnan";
+
+        System.out.println(str.charAt(1));
     }
 }
