f68acd12f3dc40bb391dacafe8c5aaab8a14b1b2
Adnan Khan <adnanuitit@gmail.com>
2026-09-10 08:53:00 +0530

    Fixed Insertion Sort Code

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -1,24 +1,27 @@
 package DSAPractice.Sorting;
 
+import java.util.Arrays;
+
 public class selectionSort {
-    public static void selectionSort(int[] arr){
+    public static int[] selectionSort(int[] arr){
+        int temp = 0;
         for (int i = 0; i <= arr.length-2; i++){
-            int min = arr[i];
+            int min = i;
             for (int j = i; j<=arr.length-2; j++){
-                if (min>arr[i]){
-                    min = arr[i];
+                if (arr[min]>arr[j]){
+                    min = j;
                 }
-                else {
-                    min = min;
-                }
-                System.out.println(min);
             }
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
+        return arr;
     }
 
 
     public static void main(String[] args) {
         int[] arr = {2,5,7,4,9,1,6};
-        selectionSort(arr);
+        System.out.println(Arrays.toString(selectionSort(arr)));
     }
 }
